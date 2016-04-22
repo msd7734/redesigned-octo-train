7a5430226db84f0fd4bddc5819da47b5c0e58794
Matt Dennis <msd7734@rit.edu>
2016-04-22 15:15:44 -0400

    Don't need to read from file after all

--- io.asm
+++ io.asm
@@ -27,34 +27,3 @@ read_int:
 	li		$v0, READ_INT
 	syscall
 	jr		$ra
-	
-#############################
-# read_file
-# Read contents of a file into a buffer.
-# Args:
-#  a0 - Length of the side of a (square) board.
-# Returns:
-#  v0 - ptr to 0 if the length is unsupported, or ptr to a row_listing
-#       in the following format:
-#		struct row_listing {
-# 			int16 count;
-# 			int16 row_data[count];
-# 		}
-#############################
-read_file:
-	addi	$sp, $sp, -8
-	sw		$s0, 0($sp)
-	sw		$s1, 4($sp)
-	# comprised of a sequence of FOPEN, FWRITE, FCLOSE
-	# shouldn't need flags for a simple read
-	
-	# take a0 as name
-	move	$a1, $zero
-	li		$a2, O_RDONLY
-	li		$v0, FOPEN
-	syscall
-	
-	
-	lw		$s0, 0($sp)
-	lw		$s1, 4($sp)
-	jr		$ra
